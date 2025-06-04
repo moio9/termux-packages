31e8022ce50caaee2c09149f80f04656056d3ac7
moio9 <111084587+moio9@users.noreply.github.com>
2025-06-04 23:02:51 +0300

    Update build.sh

--- packages/vulkan-wrapper-android/build.sh
+++ packages/vulkan-wrapper-android/build.sh
@@ -54,7 +54,7 @@ EOF
 	cd subprojects
 	git clone --recurse-submodules https://github.com/Pipetto-crypto/libadrenotools.git
 
-	_target_file="$TERMUX_PKG_SRCDIR/src/vulkan/wsi/wsi_common_x11.c"
+	target_file="$TERMUX_PKG_SRCDIR/src/vulkan/wsi/wsi_common_x11.c"
 	if [ -f "$_target_file" ]; then
 		sed -i '/typedef struct.*native_handle/,/} native_handle_t;/s/^/\/\//' "$target_file"
 	fi
