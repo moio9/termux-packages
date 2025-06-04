c436fd272297a3bd2ce59e42163655636d3122b8
moio9 <111084587+moio9@users.noreply.github.com>
2025-06-05 00:01:10 +0300

    Update build.sh

--- packages/vulkan-wrapper-android/build.sh
+++ packages/vulkan-wrapper-android/build.sh
@@ -55,11 +55,16 @@ EOF
 	git clone --recurse-submodules https://github.com/Pipetto-crypto/libadrenotools.git
 
 	# ----------- PATCH NATIV HANDLE -----------
+
 	target_file="$TERMUX_PKG_SRCDIR/src/vulkan/wsi/wsi_common_x11.c"
 	if [ -f "$target_file" ]; then
 		sed -i '/typedef struct/,/} native_handle_t;/d' "$target_file"
-		sed -i '1i #include <android_stub/cutils/native_handle.h>' "$TERMUX_PKG_SRCDIR/include/android_stub/vndk/hardware_buffer.h"
 	fi
+
+	sed -i '1i #include <android_stub/cutils/native_handle.h>' "$TERMUX_PKG_SRCDIR/include/android_stub/vndk/hardware_buffer.h"
+
+	sed -i '1i #include <android_stub/cutils/native_handle.h>' "$TERMUX_PKG_SRCDIR/src/vulkan/wrapper/wrapper_device_memory.c"
+	
 	# ------------------------------------------
 
 	cd "$TERMUX_PKG_SRCDIR"
