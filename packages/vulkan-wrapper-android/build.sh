f5fd5dfa3f613c174de5d632919a5bd66e1402b6
moio9 <111084587+moio9@users.noreply.github.com>
2025-06-04 22:49:03 +0300

    Update build.sh

--- packages/vulkan-wrapper-android/build.sh
+++ packages/vulkan-wrapper-android/build.sh
@@ -56,7 +56,7 @@ EOF
 
 	_target_file="$TERMUX_PKG_SRCDIR/src/vulkan/wsi/wsi_common_x11.c"
 	if [ -f "$_target_file" ]; then
-		sed -i '/#ifdef __TERMUX__/,/#include <sys\/socket.h>/s/^/\/\//' "$_target_file"
+		sed -i '/typedef struct.*native_handle/,/} native_handle_t;/s/^/\/\//' "$target_file"
 	fi
 
 	cd "$TERMUX_PKG_SRCDIR"
