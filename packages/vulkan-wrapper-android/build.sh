dd2cecbd5b7318c21585f0322ce83c681453b4b7
moio9 <111084587+moio9@users.noreply.github.com>
2025-06-04 04:34:24 +0300

    Update build.sh

--- packages/vulkan-wrapper-android/build.sh
+++ packages/vulkan-wrapper-android/build.sh
@@ -43,6 +43,7 @@ termux_step_post_get_source() {
 	sed -i '/libadrenotools_dep/d' src/vulkan/wrapper/meson.build
 	sed -i '/adrenotools_subproj/d' src/vulkan/wrapper/meson.build
 
+	sed -i 's|#include <cutils/native_handle.h>|// removed native_handle.h|g' src/vulkan/wsi/wsi_common_x11.c
 	sed -i '73d' src/vulkan/wsi/wsi_common_x11.c
 }
 
