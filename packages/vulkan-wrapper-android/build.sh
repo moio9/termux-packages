390a8237b8b214063677ba2e9a1110b6955a1d64
moio9 <111084587+moio9@users.noreply.github.com>
2025-06-05 00:32:57 +0300

    Update build.sh

--- packages/vulkan-wrapper-android/build.sh
+++ packages/vulkan-wrapper-android/build.sh
@@ -55,10 +55,15 @@ EOF
 	git clone --recurse-submodules https://github.com/Pipetto-crypto/libadrenotools.git
 
 	find "$TERMUX_PKG_SRCDIR" \( -name '*.c' -o -name '*.h' \) \
+		-exec sed -i '/typedef struct native_handle/,/} native_handle_t;/d' {} +
+
+	find "$TERMUX_PKG_SRCDIR" \( -name '*.c' -o -name '*.h' \) \
 		-exec grep -l 'native_handle_t' {} \; \
 		| while read -r f; do
-
-			if [[ "$f" != *cutils/native_handle.h* ]] && ! grep -q 'android_stub/cutils/native_handle.h' "$f"; then
+			case "$f" in
+				*android_stub/cutils/native_handle.h*) continue ;;
+			esac
+			if ! grep -q 'android_stub/cutils/native_handle.h' "$f"; then
 				sed -i '1i #include <android_stub/cutils/native_handle.h>' "$f"
 			fi
 		done
